6bafef7d7cb6153c1104e4e5440c142950f51b02
MaiLinh <vanmailinh.10@gmail.com>
2024-11-22 23:19:06 +0700

    Tien do tuan 2

---
+++ Văn Thị Mai Linh/Product/SEABATTLE/src/Board.java
@@ -0,0 +1,134 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Board {
+    private int[][] board = new int[15][15];
+    private int[][] fogBoard = new int[15][15];
+
+
+    private ArrayList<int[]> ship = new ArrayList<int[]>();// danh sach toa do 1 thuyen
+    private ArrayList<Ship> ships = new ArrayList<Ship>();// danh sach luu cac thuyen
+    private ArrayList<Boolean>hitStatus = new ArrayList<Boolean>(); // luu trang thai cua cac o ( da bi ban trung hay chua)
+    // khoi tao thong so ban dau
+    public Board(int[][] board, int [][]fogBoard, ArrayList<Ship>ships){
+        this.board = board;
+        this.fogBoard = fogBoard;
+        this.ships = ships;
+    }
+
+    public void setBoard(){
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++)
+                board[i][j] = 0; // ban dau bang chua co thuyen
+        }
+    }
+    public void setFogBoard(){
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++)
+                fogBoard[i][j] = 0; // ban dau bang chua co thuyen
+        }
+    }
+//    public boolean isSunk(ArrayList<Boolean>hitStatus){
+//        return !hitStatus.contains(false);// neu o nao cung bi ban thi thuyen chim
+//    }
+
+    Scanner scanf = new Scanner(System.in);
+    // dat thuyen
+    public void setShip(){
+
+        System.out.println("Nhập tên của thuyền: ");
+        String name = scanf.nextLine();
+
+        System.out.println("Nhập toạ độ vị trí đầu của thuyền: ");
+        int x_begin = scanf.nextInt();
+        int y_begin = scanf.nextInt();
+        scanf.nextLine();
+
+        System.out.println("Nhập toạ độ vị trí cuối của thuyền: ");
+        int x_end = scanf.nextInt();
+        int y_end = scanf.nextInt();
+        scanf.nextLine();
+
+        ship = new ArrayList<int[]>();
+        for (int i = x_begin; i <= x_end; i++) {
+            for (int j = y_begin; j <= y_end; j++) {
+                ship.add(new int[]{i, j});
+                board[i][j] = 1;
+            }
+        }
+        ships.add(new Ship(name, ship, hitStatus));
+        System.out.println("Đã thêm thuyền thành công: ");
+    }
+    // tan cong
+    public void attackShip(Player player, Player enemy){
+            boolean check = false;
+            System.out.println("Nhập toạ độ ô mà bạn muốn bắn: ");
+            int x = scanf.nextInt();
+            int y = scanf.nextInt();
+            scanf.nextLine();
+            Board enemyBoard = enemy.getBoard();
+            for (Ship boat : enemyBoard.ships) {
+                if(boat.isOccupyCoordinate(x,y)){
+                    check = true;
+                    enemyBoard.board[x][y] = 2;
+                    enemyBoard.fogBoard[x][y] = 2;
+                    player.increaseCellEnermyHited();
+                    if(boat.isSunk()){
+                        System.out.println("Bạn đã bắn chìm tàu: " + boat.getNameShip());
+                        player.increaseDestroyedShip();
+                        enemy.decreaseShipLeft();
+                        System.out.println("Xin mời lượt bắn tiếp theo");
+                    }
+                    else
+                        System.out.println("Bạn đã bắn trúng tàu. Xin mời lượt bắn tiếp theo");
+                    break;
+                }
+            }
+            if(check){
+                attackShip(player, enemy);
+            }
+            else{
+                enemyBoard.board[x][y] = -1;
+                enemyBoard.fogBoard[x][y] = -1;
+                System.out.println("Bạn đã bắn trượt, đến lượt đối thủ.");
+            }
+
+    }
+
+    // hien thi bang
+    public void displayBoard(){
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                if(board[i][j] == 0)
+                    System.out.printf("~ ");
+                else if(board[i][j] == 1)
+                    System.out.printf("O ");
+                else
+                    System.out.printf("X ");
+            }
+            System.out.println();
+        }
+    }
+    // hien thi bang an
+    public void displayFogBoard(){
+        for(int i = 1; i <= 10; i++){
+            for(int j = 1; j <= 10; j++){
+                if(fogBoard[i][j] == 0)
+                    System.out.printf("~ ");
+                else if(fogBoard[i][j] == -1)
+                    System.out.printf("O ");
+                else if(fogBoard[i][j] == 2)
+                    System.out.printf("X ");
+            }
+            System.out.println();
+        }
+    }
+    // kiem tra game ket thuc chua
+    public boolean isGameover(){
+        for(Ship ship: ships){
+            if(!ship.isSunk())
+                return false;
+        }
+        return true;
+    }
+
+}
---
+++ Văn Thị Mai Linh/Product/SEABATTLE/src/Game.java
@@ -0,0 +1,134 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Game {
+    Scanner scanf = new Scanner(System.in);
+    // nguoi choi 1
+    private int [][] board1 = new int [15][15];
+    private int[][] fogBoard1 = new int [15][15];
+    private ArrayList<Ship>ships1 = new ArrayList<Ship>();
+    Board boardA = new Board(board1, fogBoard1,ships1);
+    Player playerA = new Player(boardA);
+    Menu player1 = new Menu(playerA);
+
+    // nguoi choi 2
+    private int [][] board2 = new int [15][15];
+    private int[][] fogBoard2 = new int [15][15];
+    private ArrayList<Ship>ships2 = new ArrayList<Ship>();
+    Board boardB = new Board(board2, fogBoard2, ships2);
+    Player playerB = new Player(boardB);
+    Menu player2 = new Menu(playerB);
+
+    public void menu(){
+        System.out.println("Bạn có các lựa chọn sau: ");
+        System.out.println("1. Hiển thị tình hình hiện tại của bạn (bao gồm: số ô đã bắn đối thủ, số tàu đã phá của đối thủ, số tàu còn lại của mình)");
+        System.out.println("2. Xem bảng đặt thuyền của bản thân.");
+        System.out.println("3. Xem bảng dạng sương mù của đối thủ.");
+        System.out.println("4. Đặt lệnh khai hoả (tấn công).");
+        System.out.println("5. Kết thúc lượt.");
+    }
+
+    public void startGame(){
+        playerA.getBoard().setBoard();
+        playerA.getBoard().setFogBoard();
+        playerB.getBoard().setBoard();
+        playerB.getBoard().setFogBoard();
+        System.out.println("Các người chơi đặt thuyền.");
+        System.out.println("Người chơi thứ nhất đặt thuyền: ");
+        int totleNum = 0;
+        while(totleNum < 5 ){
+            boardA.setShip();
+            totleNum++;
+        }
+        System.out.println("Người chơi thứ 2 đặt thuyền: ");
+        totleNum = 0;
+        while(totleNum < 5){
+            boardB.setShip();
+            totleNum++;
+        }
+    }
+
+    public void playGame() {
+        boolean gameRunning = true;
+        int currentPlayer = 1;
+        int winner = 1;
+        while (gameRunning) {
+            if (currentPlayer == 1) {
+                System.out.println("Lượt của người thứ nhất: ");
+                menu();
+                while (true) {
+                    System.out.println("Nhập lựa chọn của bạn: ");
+                    int choice = scanf.nextInt();
+                    scanf.nextLine();
+                    switch (choice) {
+                        case 1:
+                            player1.currentStatus();
+                            break;
+                        case 2:
+                            player1.viewTable();
+                            break;
+                        case 3:
+                            player2.viewFogTable();
+                            break;
+                        case 4:
+                            player1.attack(playerB);
+                            break;
+                        case 5:
+                            currentPlayer = 2;
+                            break;
+                        default:
+                            System.out.println("Lựa chọn không hợp lệ, vui lòng chọn lại.");
+                    }
+                    if(choice == 5)
+                        break;
+                }
+            }
+            if (currentPlayer == 2) {
+                System.out.println("Lượt của người thứ hai: ");
+                menu();
+                while (true) {
+                    System.out.println("Nhập lựa chọn của bạn: ");
+                    int choice = scanf.nextInt();
+                    scanf.nextLine();
+
+                    switch (choice) {
+                        case 1:
+                            player2.currentStatus();
+                            break;
+                        case 2:
+                            player2.viewTable();
+                            break;
+                        case 3:
+                            player1.viewFogTable();
+                            break;
+                        case 4:
+                            player2.attack(playerA);
+                            break;
+                        case 5:
+                            currentPlayer = 1;
+                            break;
+                        default:
+                            System.out.println("Lựa chọn không hợp lệ, vui lòng chọn lại.");
+                    }
+                    if(choice == 5)
+                        break;
+                }
+            }
+            if(boardA.isGameover()){
+                winner = 2;
+                gameRunning = false;
+            }
+            else if(boardB.isGameover()){
+                winner = 1;
+                gameRunning = false;
+            }
+        }
+        System.out.println("Trận chiến kết thúc, người chiến thắng là người thứ: " + winner);
+        System.out.println("Bảng chơi của người chơi 1: ");
+        boardA.displayBoard();
+        System.out.println("Bảng chơi của người chơi 2: ");
+        boardB.displayBoard();
+    }
+
+
+
+}
