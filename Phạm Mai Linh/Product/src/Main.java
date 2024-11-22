9e58a0b9ee76f66f935902cc7c4cac896b32299b
tolalinhne <plinh7009@gmail.com>
2024-11-22 20:38:56 +0700

    check tien do lan 2

---
+++ Phạm Mai Linh/Product/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        SeaBattleGame game = new SeaBattleGame();
+        game.startGame();
+    }
+}
---
+++ Phạm Mai Linh/Product/src/Ship.java
@@ -0,0 +1,17 @@
+public class Ship {
+    private int size;
+    private char symbol;
+
+    public Ship(int size, char symbol) {
+        this.size = size;
+        this.symbol = symbol;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+}
