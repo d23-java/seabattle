564be936c97804407c0a363893a3b3503a98a1ed
Dũng Nguyễn <153367071+DungNguyenCoder@users.noreply.github.com>
2024-11-22 16:35:00 +0700

    Delete d23_nguyen_manh_dung directory

--- d23_nguyen_manh_dung/Product/src/test.java
+++
@@ -1,9 +0,0 @@
-import ui.TextScreen;
-import utilz.LoadSave;
-
-public class test {
-    public static void main(String[] args) {
-        LoadSave.loadLeaderboard();
-        TextScreen.displayLeaderboard();
-    }
-}
