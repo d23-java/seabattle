3f663db322d34b77da35788769780037893236b8
HieuGM <duchieutuyen@gmail.com>
2024-11-26 00:42:56 +0700

    Tien do lan 2

---
+++ Giáp Minh Hiếu/Product/src/Menu.java
@@ -0,0 +1,16 @@
+public class Menu {
+
+    public Menu() {};
+
+    public void displayMenuStart() {
+        System.out.println("SEA BATTLE");
+        System.out.println("Enter your choice: ");
+        System.out.println("1. Start a new game");
+        System.out.println("2. Exit");
+    }
+
+    public void newGame() {
+        System.out.println("Enter your name: ");
+    }
+
+}
